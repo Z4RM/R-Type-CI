3f9297ef5f40b15497241d538930e97f25956a28
Clement-Z4RM <cloviswakfu@gmail.com>
2024-11-26 19:17:23 +0100

    build: setup build for R-Type server, client and common (library) and setup CMake and Conan (package manager)

---
+++ client/include/foo.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** R_Type
+** File description:
+** foo.hpp
+*/
+
+#ifndef FOO_HPP_
+#define FOO_HPP_
+
+// TODO: this is a sample file, remove it once a real one is created
+
+#endif /* !FOO_HPP_ */
---
+++ common/src/foo.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** main.cpp
+*/
+
+// TODO: this is a sample file, remove it once a real one is created
