019bf6b45cbdb94e4764a25ed15201d978dd9466
MathisZUCCHERO <mathis.zucchero@epitech.eu>
2024-12-04 13:29:43 +0100

    feat(common): add doxygen documentation

--- src/ECS/ComponentManager.hpp
+++ src/ECS/ComponentManager.hpp
@@ -13,8 +13,25 @@
 #include <typeindex>
 #include "SparseSet.hpp"
 
+/**
+ * @class ComponentManager
+ * @brief Manages components associated with entities in an Entity-Component-System (ECS).
+ *
+ * The `ComponentManager` class allows adding, removing, and retrieving generic type components
+ * associated with entities identified by unique IDs. Components are stored in specialized sparse sets (`SparseSet`) for each type.
+ */
 class ComponentManager {
 public:
+    /**
+     * @brief Adds a component to an entity.
+     *
+     * If the component type does not yet exist, a new sparse set (`SparseSet`) is created for that type.
+     * The component is then added to the specified entity.
+     *
+     * @tparam T The type of the component.
+     * @param entity The unique identifier of the entity.
+     * @param component The component to add.
+     */
     template <typename T>
     void addComponent(unsigned int entity, const T& component) {
         if (_componentSets.find(typeid(T)) == _componentSets.end()) {
@@ -24,6 +41,15 @@ public:
         sparse_set->addComponent(entity, component);
     }
 
+    /**
+     * @brief Removes a component from an entity.
+     *
+     * If a sparse set for the component type exists, the component associated with the specified
+     * entity is removed.
+     *
+     * @tparam T The type of the component.
+     * @param entity The unique identifier of the entity.
+     */
     template <typename T>
     void removeComponent(unsigned int entity) {
         if (_componentSets.find(typeid(T)) != _componentSets.end()) {
@@ -32,6 +58,16 @@ public:
         }
     }
 
+    /**
+     * @brief Retrieves a component associated with an entity.
+     *
+     * If a sparse set for the component type exists and the entity has a component of that type,
+     * it is returned. Otherwise, returns `nullptr`.
+     *
+     * @tparam T The type of the component.
+     * @param entity The unique identifier of the entity.
+     * @return A unique pointer to the component, or `nullptr` if the entity does not have a component of this type.
+     */
     template <typename T>
     std::unique_ptr<T> getComponent(unsigned int entity) {
         if (_componentSets.find(typeid(T)) != _componentSets.end()) {
@@ -40,7 +76,14 @@ public:
         }
         return nullptr;
     }
+
 private:
+    /**
+     * @brief A mapping of component types to sparse sets (`SparseSet`).
+     *
+     * The keys in the map are type indices (`std::type_index`), and the values are
+     * shared pointers (`std::shared_ptr<void>`) to the corresponding sparse sets.
+     */
     std::unordered_map<std::type_index, std::shared_ptr<void>> _componentSets;
 };
 
--- src/ECS/SparseSet.hpp
+++ src/ECS/SparseSet.hpp
@@ -11,9 +11,27 @@
 #include <vector>
 #include <unordered_map>
 
+/**
+ * @class SparseSet
+ * @brief Manages a sparse set for efficient storage and retrieval of components.
+ *
+ * The `SparseSet` class is designed to handle components associated with entities in an ECS (Entity-Component-System).
+ * It provides efficient methods for adding, removing, and accessing components, while keeping track of associated entities.
+ *
+ * @tparam T The type of the components to store.
+ */
 template <typename T>
 class SparseSet {
 public:
+    /**
+     * @brief Adds a component for a given entity.
+     *
+     * If the entity does not already have a component, it is added to the dense array, and its
+     * index is stored in the sparse map. The associated component is stored in a separate dense array.
+     *
+     * @param entity The unique ID of the entity.
+     * @param component The component to associate with the entity.
+     */
     void addComponent(unsigned int entity, const T& component) {
         if (_sparse.find(entity) == _sparse.end()) {
             _sparse[entity] = _dense.size();
@@ -22,6 +40,14 @@ public:
         }
     }
 
+    /**
+     * @brief Removes the component associated with a given entity.
+     *
+     * If the entity has an associated component, it is removed. The dense array and
+     * sparse map are updated to maintain efficiency.
+     *
+     * @param entity The unique ID of the entity.
+     */
     void removeComponent(unsigned int entity) {
         if (_sparse.find(entity) != _sparse.end()) {
             size_t index = _sparse[entity];
@@ -38,6 +64,15 @@ public:
         }
     }
 
+    /**
+     * @brief Retrieves the component associated with a given entity.
+     *
+     * If the entity has an associated component, a unique pointer to the component is returned.
+     * Otherwise, returns `nullptr`.
+     *
+     * @param entity The unique ID of the entity.
+     * @return A unique pointer to the component, or `nullptr` if the entity does not have a component.
+     */
     std::unique_ptr<T> getComponent(unsigned int entity) {
         if (_sparse.find(entity) != _sparse.end()) {
             return std::make_unique<T>(_components[_sparse[entity]]);
@@ -45,13 +80,37 @@ public:
         return nullptr;
     }
 
+    /**
+     * @brief Gets the list of entities currently stored in the set.
+     *
+     * Returns a reference to the dense array containing the IDs of all entities
+     * with associated components.
+     *
+     * @return A reference to the vector of entity IDs.
+     */
     const std::vector<unsigned int>& getEntities() const {
         return _dense;
     }
 
 private:
+    /**
+     * @brief A mapping from entity IDs to their indices in the dense array.
+     */
     std::unordered_map<unsigned int, size_t> _sparse;
+
+    /**
+     * @brief A dense array of entity IDs.
+     *
+     * Contains the IDs of all entities with components, stored in a compact form
+     * for efficient iteration.
+     */
     std::vector<unsigned int> _dense;
+
+    /**
+     * @brief A dense array of components.
+     *
+     * Stores components in the same order as the corresponding entities in `_dense`.
+     */
     std::vector<T> _components;
 };
 
