b1fe1b3056f08caffbe0f24beb737518bb30df67
josephine <josephine.chicouras@epitech.eu>
2024-12-08 12:06:22 +0100

    feat(client/game/base): add functions to open a window with the background and modify constructor to set scale of the background thanks to the size of the window

--- src/RType/Game/Game.cpp
+++ src/RType/Game/Game.cpp
@@ -11,12 +11,42 @@
 rtype::Game::Game(int width, int height, const std::string &title) : _window(sf::VideoMode(width, height), title)
 {
     _window.setFramerateLimit(60);
-    if (!_backgroundTexture.loadFromFile("../../../sprites/background.jpg")) {
+    if (!_backgroundTexture.loadFromFile("sprites/background.jpg")) {
         std::cerr << "Error : Impossible to load 'background.jpg'." << std::endl;
         throw std::runtime_error("Texture not found");
     }
     _backgroundSprite.setTexture(_backgroundTexture);
+    sf::Vector2u textureSize = _backgroundTexture.getSize();
+    sf::Vector2u windowSize = _window.getSize();
+
+    float scaleX = static_cast<float>(windowSize.x) / textureSize.x;
+    float scaleY = static_cast<float>(windowSize.y) / textureSize.y;
+    _backgroundSprite.setScale(scaleX, scaleY);
 }
 
 rtype::Game::~Game() {}
 
+void rtype::Game::run()
+{
+    while (_window.isOpen()) {
+        processEvents();
+        update();
+        render();
+    }
+}
+
+void rtype::Game::processEvents() {
+    sf::Event event{};
+    while (_window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            _window.close();
+    }
+}
+
+void rtype::Game::update() {}
+
+void rtype::Game::render() {
+    _window.clear();
+    _window.draw(_backgroundSprite);
+    _window.display();
+}
