d7fa74dab058187fe8adf0c57323c58794a680d6
josephine <josephine.chicouras@epitech.eu>
2024-12-08 11:42:42 +0100

    feat(client/game/base): add constructor for the Game's class

---
+++ src/RType/Game/Game.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Game.cpp
+*/
+
+#include "Game.hpp"
+#include <iostream>
+
+rtype::Game::Game(int width, int height, const std::string &title) : _window(sf::VideoMode(width, height), title)
+{
+    _window.setFramerateLimit(60);
+    if (!_backgroundTexture.loadFromFile("../../../sprites/background.jpg")) {
+        std::cerr << "Error : Impossible to load 'background.jpg'." << std::endl;
+        throw std::runtime_error("Texture not found");
+    }
+    _backgroundSprite.setTexture(_backgroundTexture);
+}
