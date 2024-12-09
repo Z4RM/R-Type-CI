2edd0bb31eeda63ca94b994815c41a93f1d473e1
Clement-Z4RM <cloviswakfu@gmail.com>
2024-12-09 01:12:40 +0100

    feat: add `RType` class with entry point static method (`RType::run`)
    All code of the R-Type should be come from this function and it should be the only statement in the program main function.

---
+++ src/RType/RType.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** RType.cpp
+*/
+
+#include "RType/Config/Config.hpp"
+#include "RType.hpp"
+
+int rtype::RType::run() {
+    Config config;
+
+    return 0;
+}
