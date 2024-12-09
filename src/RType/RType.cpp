b405f75b4c9412c1f980810994b83fb530a04d34
MathisZUCCHERO <mathis.zucchero@epitech.eu>
2024-12-09 12:55:10 +0100

    feat(main): add initialization of all manager

--- src/RType/RType.cpp
+++ src/RType/RType.cpp
@@ -7,8 +7,15 @@
 
 #include "RType/Config/Config.hpp"
 #include "RType.hpp"
+#include "ECS.hpp"
+#include "Systems.hpp"
+#include "Components/Player.hpp"
 
 int rtype::RType::run() {
     Config::initialize();
+
+    ecs::EntityManager entityManager;
+    ecs::ComponentManager componentManager;
+    ecs::SystemManager systemManager(entityManager, componentManager);
     return 0;
 }
