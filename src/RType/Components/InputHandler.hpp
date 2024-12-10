60e12257210d1ed9e3abb4f0afac286ee2cc66da
josephine <josephine.chicouras@epitech.eu>
2024-12-10 19:37:04 +0100

    docs: add doxygen for the system that handle keys

--- src/RType/Components/InputHandler.hpp
+++ src/RType/Components/InputHandler.hpp
@@ -5,6 +5,14 @@
 ** InputHandler.hpp
 */
 
+/**
+ * @file InputHandler.hpp
+ * @brief Header file for the InputHandler component in the RType project.
+ *
+ * This file defines the `InputHandler` struct, which maps keyboard keys
+ * to specific actions represented as functions.
+ */
+
 #ifndef INPUTHANDLER_HPP
 #define INPUTHANDLER_HPP
 
@@ -13,7 +21,22 @@
 #include <SFML/Window/Keyboard.hpp>
 
 namespace rtype::components {
+    /**
+     * @struct InputHandler
+     * @brief Manages keyboard input actions for entities.
+     *
+     * The `InputHandler` component allows associating keyboard keys with
+     * specific actions using a map. This enables easy handling of input-driven
+     * behavior in the RType game.
+     */
     struct InputHandler {
+        /**
+         * @brief A map associating keyboard keys with actions.
+         *
+         * Each key in the map corresponds to an `sf::Keyboard::Key`, and each action
+         * is represented as a `std::function<void()>`. This allows flexible handling
+         * of key presses, binding specific functions to them.
+         */
         std::unordered_map<sf::Keyboard::Key, std::function<void()>> keyActions;
     };
 }
--- src/Systems/InputSystem.hpp
+++ src/Systems/InputSystem.hpp
@@ -13,8 +13,29 @@
 #include <SFML/Window/Event.hpp>
 
 namespace rtype::systems {
+    /**
+     * @class InputSystem
+     * @brief Handles input events and triggers corresponding actions.
+     *
+     * The `InputSystem` class is responsible for processing input events, such as
+     * key presses, and invoking appropriate actions based on the input. It interacts
+     * with the entity and component managers to access the game entities and their
+     * associated input handlers.
+     */
     class InputSystem {
     public:
+        /**
+         * @brief Handles the input event and triggers actions for entities.
+         *
+         * This static function processes input events and triggers the associated
+         * actions for entities that have an `InputHandler` component.
+         *
+         * @param entityManager A reference to the entity manager, used to access
+         *        the entities in the game.
+         * @param componentManager A reference to the component manager, used to
+         *        access the components of the entities.
+         * @param event The input event (e.g., keyboard key press) to process.
+         */
         static void handleInput(ecs::EntityManager &entityManager, ecs::ComponentManager &componentManager, const sf::Event &event);
     };
 }
