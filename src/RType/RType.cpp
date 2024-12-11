f318562711e3bddda8fd8afe2bdc8e8db7ffd3eb
Clement-Z4RM <cloviswakfu@gmail.com>
2024-12-11 03:12:37 +0100

    feat: start implementation of client (and `Client` class) in `RType` one [WIP]

--- src/RType/RType.cpp
+++ src/RType/RType.cpp
@@ -5,17 +5,64 @@
 ** RType.cpp
 */
 
-#include "RType/Config/Config.hpp"
-#include "RType.hpp"
+#include "ModeManager/ModeManager.hpp"
+#include "Config/Config.hpp"
 #include "ECS.hpp"
-#include "Systems.hpp"
-#include "Components/Player.hpp"
+#include "RType.hpp"
 
 int rtype::RType::run() {
-    Config::initialize();
+    if (!Config::initialize())
+        return 84;
 
+    RType rtype(Config::getInstance().getNetwork().server.port);
+
+    return rtype._run();
+}
+
+#ifdef RTYPE_IS_CLIENT
+void rtype::RType::startServer() {
+    if (IS_SERVER) {
+        spdlog::warn("Server seems to be already started, do nothing");
+        return;
+    }
+    ENABLE_SERVER();
+    // TODO
+}
+
+void rtype::RType::stopServer() {
+    if (!IS_SERVER) {
+        spdlog::warn("Server seems to be already stopped, do nothing");
+        return;
+    }
+    DISABLE_SERVER();
+    // TODO
+}
+#endif
+
+#ifndef RTYPE_IS_CLIENT
+rtype::RType::RType(ushort port) : _port(port), _server(_port) {}
+#else
+rtype::RType::RType(ushort port) : _port(port), _server(_port), _client(this) {}
+#endif
+
+int rtype::RType::_run() {
     ecs::EntityManager entityManager;
     ecs::ComponentManager componentManager;
     ecs::SystemManager systemManager(entityManager, componentManager);
+
+    while (_running()) {
+#ifdef RTYPE_IS_CLIENT
+        _client.iteration();
+#endif
+    }
     return 0;
+}
+
+bool rtype::RType::_running() {
+#ifdef RTYPE_IS_SERVER
+    return true; // TODO
+#endif
+#ifdef RTYPE_IS_CLIENT
+    return _client.running();
+#endif
 }
--- src/RType/RType.hpp
+++ src/RType/RType.hpp
@@ -8,16 +8,21 @@
 #ifndef RTYPE_RTYPE_HPP_
 #define RTYPE_RTYPE_HPP_
 
+#include "Server/Network/UDPServer/UDPServer.hpp"
+#ifdef RTYPE_IS_CLIENT
+#include "Client/Client.hpp"
+#endif
+
 namespace rtype {
     /**
      * @class RType
      *
-     * @brief The main class of the R-Type, containing, among others, the entry points.
+     * @brief The main class of the R-Type, containing, among others, the entrypoint.
      */
     class RType {
     public:
         /**
-         * @brief The entry point of the R-Type.
+         * @brief The entrypoint of the R-Type.
          * Should be the only statement in the program main function, used as is:
          * ```cpp
          * #include "RType/RType.hpp"
@@ -30,6 +35,35 @@ namespace rtype {
          * @return The exit status of the program.
          */
         static int run();
+
+#ifdef RTYPE_IS_CLIENT
+        // TODO: documentation
+        void startServer();
+
+        // TODO: documentation
+        void stopServer();
+#endif
+
+    private:
+        // TODO: documentation
+        RType(ushort port);
+
+        // TODO: documentation
+        int _run();
+
+        // TODO: documentation
+        bool _running();
+
+        // TODO: documentation
+        ushort _port;
+
+        // TODO: documentation
+        server::network::UDPServer _server;
+
+#ifdef RTYPE_IS_CLIENT
+        // TODO: documentation
+        Client _client;
+#endif
     };
 }
 
