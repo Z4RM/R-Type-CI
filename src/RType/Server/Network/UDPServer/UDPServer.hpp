eb9c7b6ae1eaad87e76fa841b3dd6f6efcde4c5c
Clement-Z4RM <cloviswakfu@gmail.com>
2024-12-11 03:10:25 +0100

    feat(server): add `UDPServer` class to perform network actions with UDP [WIP]

---
+++ src/RType/Server/Network/UDPServer/UDPServer.hpp
@@ -0,0 +1,70 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** UDPServer.hpp
+*/
+
+#ifndef RTYPE_UDPSERVER_HPP_
+#define RTYPE_UDPSERVER_HPP_
+
+#include <optional>
+#include "asio.hpp"
+
+// TODO: documentation
+#define BUFFER_SIZE 1024
+
+namespace rtype::server::network {
+    // TODO: documentation
+    class UDPServer {
+    public:
+        // TODO: documentation
+        class StartException : public std::exception {
+        public:
+            // TODO: documentation
+            StartException();
+        };
+
+        // TODO: documentation
+        UDPServer(ushort port);
+
+        // TODO: documentation
+        /**
+         * @throw StartException If the server is already started.
+         */
+        void start();
+
+        // TODO: documentation
+        void stop();
+
+    private:
+        // TODO: refactor
+        void _receive();
+
+        // TODO: refactor
+        void _send();
+
+        // TODO: documentation
+        ushort _port;
+
+        // TODO: documentation
+        std::atomic<bool> _running = false;
+
+        // TODO: documentation
+        asio::io_context _ioContext;
+
+        // TODO: documentation
+        std::optional<asio::ip::udp::socket> _socket;
+
+        // TODO: documentation
+        std::optional<asio::ip::udp::endpoint> _endpoint;
+
+        // TODO: documentation
+        std::thread _thread;
+
+        // TODO: documentation
+        std::array<char, BUFFER_SIZE> _buffer{};
+    };
+}
+
+#endif /* !RTYPE_UDPSERVER_HPP_ */
