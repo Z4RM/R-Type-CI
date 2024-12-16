93c816b46d6d8dc3997406b22908070e898692f3
MathisZUCCHERO <mathis.zucchero@epitech.eu>
2024-12-16 16:24:20 +0100

    docs(enemy): add enemy doc

--- src/RType/Components/Enemy.hpp
+++ src/RType/Components/Enemy.hpp
@@ -13,9 +13,32 @@
 #include "ECS.hpp"
 
 namespace rtype::components {
+    /**
+     * @class Enemy
+     * @brief Represents an enemy entity in the ECS framework.
+     *
+     * The `Enemy` class encapsulates the properties and behaviors of enemy entities.
+     * It includes support for both client and server configurations, with additional
+     * rendering and animation capabilities on the client side.
+     */
     class Enemy {
     public:
 #ifdef RTYPE_IS_CLIENT
+        /**
+         * @brief Constructs an enemy entity for the client-side implementation.
+         *
+         * This constructor initializes an enemy entity with its position, velocity, size,
+         * sprite, and animation components. It is used exclusively on the client side,
+         * where rendering and animations are required.
+         *
+         * @param entityManager A reference to the `EntityManager` for managing entities.
+         * @param componentManager A reference to the `ComponentManager` for managing components.
+         * @param pos The initial position of the enemy.
+         * @param vel The velocity of the enemy.
+         * @param size The size of the enemy.
+         * @param sprite The sprite representing the enemy.
+         * @param animation The animation settings for the enemy.
+         */
         Enemy(
               rtype::ecs::EntityManager &entityManager,
               rtype::ecs::ComponentManager &componentManager,
@@ -26,6 +49,19 @@ namespace rtype::components {
               const Animation &animation
         );
 #else
+        /**
+         * @brief Constructs an enemy entity for the server-side implementation.
+         *
+         * This constructor initializes an enemy entity with its position, velocity,
+         * and size components. It is used exclusively on the server side, where
+         * rendering and animations are not required.
+         *
+         * @param entityManager A reference to the `EntityManager` for managing entities.
+         * @param componentManager A reference to the `ComponentManager` for managing components.
+         * @param pos The initial position of the enemy.
+         * @param vel The velocity of the enemy.
+         * @param size The size of the enemy.
+         */
         Enemy(
               rtype::ecs::EntityManager &entityManager,
               rtype::ecs::ComponentManager &componentManager,
@@ -35,9 +71,15 @@ namespace rtype::components {
         );
 #endif
 
+        /**
+         * @brief Default destructor for the `Enemy` class.
+         */
         ~Enemy() = default;
 
     private:
+        /**
+         * @brief The unique identifier for the player entity.
+         */
         size_t _id;
     };
 }
