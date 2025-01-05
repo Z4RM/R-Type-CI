c903e465d7d2616361d093e93ff2f95b60dcd20d
Z4RM <48418157+Z4RM@users.noreply.github.com>
2025-01-05 01:26:06 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-conan profile detect
-
-conan install . --output-folder=build --build=missing -c tools.system.package_manager:mode=install
-
-cmake --preset conan-release
-
-cmake --build --preset conan-release
